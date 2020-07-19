bce3d7bae4d1991b35df7b4060e099743a2fe61c
shakori999 <shakori999@gmail.com>
2020-07-19 12:58:56 +0430

    import the framewroks and labryes

--- full_cours/game1/game1.py
+++ full_cours/game1/game1.py
@@ -1,3 +1,11 @@
-print(200)
-print(100)
+try:
+    import os
+    import sys
+    import pygame as pg
+    import random
+    import math
+    import getopt
+    from pygame.locals import *
+except ImportError as err:
+    print("this moudle didn't import", err)
 
