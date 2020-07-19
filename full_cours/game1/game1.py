3637189e14abb64867e69e12ea9802654ad2a5f5
shakori999 <shakori999@gmail.com>
2020-07-19 13:06:06 +0430

    make the simple screen

--- full_cours/game1/game1.py
+++ full_cours/game1/game1.py
@@ -9,3 +9,22 @@ try:
 except ImportError as err:
     print("this moudle didn't import", err)
 
+## make the screen and main function
+def main():
+    pg.init()
+    black = 0, 0, 0
+    # setup the screen
+    size = width, height = 500, 500
+    screen = pg.display.set_mode(size)
+
+    while 1:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+
+        screen.fill(black)
+        pg.display.flip()
+
+
+if __name__ == "__main__":
+    main()
